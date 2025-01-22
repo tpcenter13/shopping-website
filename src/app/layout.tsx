da28c6ab235f946a448dee6bf64159cbcfb1947d
unknown <tpcenter13@gmail.com>
2025-01-22 13:46:05 +0800

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Navbar from "@/components/Navbar";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import NewProducts from "@/components/NewProducts";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
